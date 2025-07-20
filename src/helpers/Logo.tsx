28dd26604b05e4abe135cd7bfdbcb08b80590585
Sairam Pasupuleti <sairampasupuleti.42@gmail.com>
2025-07-20 11:52:19 +0530

    MODIFY | Logo config, lint

--- src/helpers/Logo.tsx
+++ src/helpers/Logo.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import logoImage from '../assets/logo/transparent/logo-primary.png';
+import footerLogo from '../assets/logo/transparent/logo-primary.png';
 interface LogoProps {
+  at?: string;
   width?: number | string;
   height?: number | string;
   className?: string;
   alt?: string;
 }
 
-const Logo: React.FC<LogoProps> = ({ 
+const Logo: React.FC<LogoProps> = ({
+  at =  logoImage,
   width = 240, 
   height = 60, 
   className = "",
@@ -16,7 +19,7 @@ const Logo: React.FC<LogoProps> = ({
   return (
     <span className={`font-heading font-bold ${className}`}>
       <img 
-        src="src/assets/logo/logo-primary.png" 
+        src={at=== 'footer-logo' ? footerLogo: logoImage}
         alt={alt}
         width={width}
         height={height}
